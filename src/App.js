c397cfb73bb4db92a44584e1ba4f6cefa01b440e
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2023-01-12 12:00:06 -0300

    Context API e eslint

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Overlay from './components/popup/Overlay';
 function App() {
   return (
     <>
-      <div><h1 onClick={()=>{alert('teste')}}>Conteudo da pagina</h1></div>
+      <div><h1 onClick={()=>{alert('teste');}}>Conteudo da pagina</h1></div>
       <Overlay />
     </>
   );
--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,34 +1,36 @@
-import React from "react";
+import React, { useContext } from 'react';
 import './Popup.css';
-import { TextField } from "@material-ui/core";
+import { TextField } from '@material-ui/core';
+import { AppConfigContext } from '../../contexts/index';
 
-const Popup = (props) => {
-    let overlay = {visibility:'hidden', opacity:0}
-    let config = props.config
-    let layerOne = config.layerOne
-    let layerTwo = config.layerTwo
+const Popup = () => {
+  let overlay = {visibility:'hidden', opacity:0};
+  const {configStyle, setConfigStyle} = useContext(AppConfigContext);
+  let config = configStyle.popup;
+  let layerOne = config.layerOne;
+  let layerTwo = config.layerTwo;
 
-    const handleOverlay = () => {
-        props.useStateConfigStyle(prevProps => ({...prevProps, overlay}))
-    }
+  const handleOverlay = () => {
+    setConfigStyle(prevProps => ({...prevProps, overlay}));
+  };
 
-    return (
-        <div className="popup-container" style={config}>
-            <button  className="button-close" onClick={handleOverlay} style={{position:'absolute', right:'20px', top:'20px'}}>X</button>
-            <div className="contente-one" style={layerOne}>
+  return (
+    <div className="popup-container" style={config}>
+      <button  className="button-close" onClick={handleOverlay} style={{position:'absolute', right:'20px', top:'20px'}}>X</button>
+      <div className="contente-one" style={layerOne}>
            
-                {/* <img src={img} style={imgStyle} alt="imagem de exemplo do popup"/>  */}
-            </div>
-            <div className="contente-two" style={layerTwo}>
-                <h2>Titulo do Popup</h2>
-                <h3>Subtitulo do popup</h3>
-                <p>Teexto do popup</p>
-                <form>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                </form>
-            </div>
-        </div>
-    )
-}
+        {/* <img src={img} style={imgStyle} alt="imagem de exemplo do popup"/>  */}
+      </div>
+      <div className="contente-two" style={layerTwo}>
+        <h2>Titulo do Popup</h2>
+        <h3>Subtitulo do popup</h3>
+        <p>Teexto do popup</p>
+        <form>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Popup
+export default Popup;
